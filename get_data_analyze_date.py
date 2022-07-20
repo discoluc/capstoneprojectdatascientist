fc1dfdc59b39a7cbbcdad94863eabb653ca70913
Lucas Breinlinger (CS-42) <lucas.nm.breinlinger@bmw.de>
2022-07-20 19:14:25 +0200

    Putting all data retrieval stuff in one file

--- get_data_analyze_date.py
+++ get_data_analyze_date.py
@@ -8,6 +8,33 @@ import matplotlib.pyplot as plt
 stocklist = pd.read_csv("ticker_full.csv")
 
 
+
+def get_stock_info(ticker):
+    """
+    Get the info about a ticker e.g. name of company.
+    ticker...Input the Ticker of a certain stock
+
+    output....json with stock info
+    """
+
+    stock_info = yf.Ticker(ticker).info
+    return stock_info
+
+
+def get_hist_data(ticker, timeframe="max"):
+    """
+    Get the historical daily market data of a certain ticker for a certain period.
+    Market Date includes the open, close, high, low preices of the day and the trading volumen.
+
+    ticker... Input the ticker of the stock of interest
+    timeframe... the period of interest (1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max)
+
+    """
+
+    hist = yf.Ticker(ticker).history(period=timeframe)
+    return hist
+
+
 def generate_consol_hist_data(stocklist, timeframe="10y"):
     """
     Gets the stock price history for a user defined list of companys.
@@ -21,7 +48,7 @@ def generate_consol_hist_data(stocklist, timeframe="10y"):
     # Loop through each tickers in the ticketlist, getting the historical data
     for ticker in tickerlist:
         print(ticker)
-        df = yf.Ticker(ticker).history(period=timeframe)
+        df = get_hist_data(ticker, timeframe)
         df["Ticker"] = ticker
         df["Date"] = df.index
         df["Name"] = stocklist[stocklist["Ticker"] == ticker]["Name"]
@@ -39,11 +66,12 @@ def generate_consol_hist_data(stocklist, timeframe="10y"):
         "Stock Splits",
     ]
     output_hist = pd.DataFrame(acc, columns=col_names)
-    output_hist.to_csv("stock_prices_10yrs.csv")
-    return outpust_hist
+    output_hist["Date"] = pd.to_datetime(output_hist["Date"])
+    output_hist.to_csv("stock_prices_10yrs.csv", index=False)
+    return output_hist
 
 
-df = generate_consol_hist[stocklist]
+df = generate_consol_hist_data(stocklist)
 
 
 # How many tickers are in the dataset.
@@ -73,12 +101,21 @@ apple.info()
 
 # Plotting the Opening and Closing Prices
 plt.figure(figsize=(15, 10))
+plt.gcf().autofmt_xdate()
 plt.plot(apple["Date"], apple["Close"], label="Close")
-plt.xlabel("Date")
-plt.ylabel("Closing Price [in USD]")
+plt.xticks(fontsize=25)
+plt.xlabel("Date", fontsize=25)
+plt.yticks(fontsize=25)
+plt.ylabel("Closing Price [in USD]", fontsize=25)
 
 # looking at the last 60 days
 plt.figure(figsize=(15, 10))
+plt.gcf().autofmt_xdate()
 plt.plot(apple["Date"].iloc[-60:], apple["Close"].iloc[-60:], label="Close")
-plt.xlabel("Date")
-plt.ylabel("Closing Price [in USD]")
+plt.xticks(fontsize=25)
+plt.xlabel("Date", fontsize=25)
+plt.yticks(fontsize=25)
+plt.ylabel("Closing Price [in USD]", fontsize=25)
+
+# Generating the Dataset as csv
+apple.to_csv("apple.csv",index = False)
